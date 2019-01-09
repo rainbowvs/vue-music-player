3c9a31558679ed969843f6cafbf5fdd6967d1294
rainbowvs <527532711@qq.com>
2019-01-09 23:49:17 +0800

    add common component Scroll

---
+++ src/coms/Scroll/Scroll.vue
@@ -0,0 +1,64 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll';
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      pullup: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this.initScroll();
+      }, 20);
+    },
+    methods: {
+      initScroll() {
+        if (!this.$refs.wrapper) {
+          return;
+        }
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        });
+      },
+      refresh() {
+        this.scroll && this.scroll.refresh();
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
