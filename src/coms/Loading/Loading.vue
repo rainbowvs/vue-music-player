ca2af0eab8d70530f8a05fcb523b70498346f95a
rainbowvs <527532711@qq.com>
2019-01-11 00:21:59 +0800

    add common component Loading

---
+++ src/coms/Loading/Loading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loading">
+    <div class="box"></div>
+    <p v-text="title"></p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '正在载入...'
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .loading {
+    @include flex(column, space-around);
+    width: 100%;
+    height: 1.5rem;
+    .box {
+      @include border(3, $color-text, top right bottom);
+      animation: rotate .5s linear infinite;
+      width: .5rem;
+      height: .5rem;
+    }
+    p {
+      margin: 0;
+      color: $color-text;
+    }
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
