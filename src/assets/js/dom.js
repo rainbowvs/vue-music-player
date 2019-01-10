59b55b2d983d30aaedce8c66adeae4e8530c1e7d
rainbowvs <527532711@qq.com>
2019-01-11 00:18:26 +0800

    add comments

--- src/assets/js/dom.js
+++ src/assets/js/dom.js
@@ -1,8 +1,20 @@
+/**
+ * 判断是否含有指定类名
+ * @param {Element} el DOM元素
+ * @param {string} className 指定类名
+ * @returns {boolean} true = 包含
+ */
 export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)');
   return reg.test(el.className);
 }
 
+/**
+ * 添加指定类名
+ * @param {Element} el DOM元素
+ * @param {string} className 类名
+ * @returns {void}
+ */
 export function addClass(el, className) {
   if (hasClass(el, className)) {
     return;
