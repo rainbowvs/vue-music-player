b8c5504fd2eaece2a124a35b260e42d679e2295d
rainbowvs <527532711@qq.com>
2019-01-13 00:59:57 +0800

    add common component SongList

---
+++ src/coms/SongList/SongList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li
+        class="item"
+        v-for="(song, index) in songs"
+        :key="index"
+      >
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      songs: {
+        type: Array,
+        default: () => []
+      }
+    },
+    methods: {
+      getDesc(song) {
+        return `${song.singer} · ${song.album}`;
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  .song-list {
+    .item {
+      @include flex(row, flex-start);
+      box-sizing: border-box;
+      height: 1.28rem;
+      .content {
+        flex: 1;
+        line-height: .4rem;
+        overflow: hidden;
+        .name {
+          @include ellipsis();
+          margin: 0;
+          font-size: $font-size-medium-x;
+          font-weight: normal;
+          color: $color-text;
+        }
+        .desc {
+          @include ellipsis();
+          margin-top: .08rem;
+          color: $color-text-d;
+          font-size: $font-size-medium;
+        }
+      }
+    }
+  }
+</style>
+
