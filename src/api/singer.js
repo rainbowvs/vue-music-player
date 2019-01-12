3dac169785f1e83f734d735d7a9e3d32e1c24a03
rainbowvs <527532711@qq.com>
2019-01-13 00:55:00 +0800

    add function requestSingerDetail

--- src/api/singer.js
+++ src/api/singer.js
@@ -2,7 +2,7 @@ import jsonp from 'assets/js/jsonp';
 import { commonParams, options } from './config';
 
 /**
- * jsonp请求歌手页的数据
+ * jsonp请求歌手列表的数据
  * @returns {Promise} promise
  */
 export function requestSinger() {
@@ -16,6 +16,27 @@ export function requestSinger() {
     hostUin: 0,
     needNewCode: 0,
     platform: 'yqq'
+  });
+  return jsonp(url, data, options);
+}
+
+/**
+ * jsonp请求指定id歌手详情的数据
+ * @param {string} singerId 歌手id
+ * @returns {Promise} promise
+ */
+export function requestSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg';
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 80,
+    songstatus: 1,
+    singermid: singerId
   });
   return jsonp(url, data, options);
 }
