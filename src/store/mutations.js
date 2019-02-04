b2d8b8786280229e93eb0ae1b7aca389fa98de02
rainbowvs <527532711@qq.com>
2019-02-04 12:32:02 +0800

    add state mutation getter

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,8 +1,54 @@
 import * as types from './mutation-types';
 
 const mutations = {
+  /**
+   * 设置歌手
+   * @param {Object} singer 歌手
+   */
   [types.SET_SINGER](state, singer) {
     state.singer = singer;
+  },
+  /**
+   * 设置播放状态
+   * @param {boolean} flag 播放状态
+   */
+  [types.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag;
+  },
+  /**
+   * 设置全屏播放
+   * @param {boolean} flag 全屏播放
+   */
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag;
+  },
+  /**
+   * 设置播放列表
+   * @param {Array} list 播放列表
+   */
+  [types.SET_PLAYLIST](state, list) {
+    state.playList = list;
+  },
+  /**
+   * 设置顺序播放列表
+   * @param {Array} list 顺序播放列表
+   */
+  [types.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list;
+  },
+  /**
+   * 设置播放模式
+   * @param {number} mode 播放模式
+   */
+  [types.SET_PLAY_MODE](state, mode) {
+    state.mode = mode;
+  },
+  /**
+   * 设置当前播放歌曲索引
+   * @param {number} index 当前播放歌曲索引
+   */
+  [types.SET_CURRENT_INDEX](state, index) {
+    state.currentIndex = index;
   }
 };
 
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import { playMode } from './config';
+
 const state = {
-  singer: {}
+  singer: {}, // 歌手
+  playing: false, // 播放状态
+  fullScreen: false, // 全屏播放状态
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序播放列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1 // 当前播放歌曲索引
 };
 
 export default state;
