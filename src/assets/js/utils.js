5541881392a25c1402a0188a96dbaa73c13e1451
rainbowvs <527532711@qq.com>
2019-02-04 12:35:08 +0800

    add pad and shuffle

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -11,3 +11,43 @@ export function serialize(obj) {
   }
   return url ? url.substring(1) : '';
 }
+
+/**
+ * 数值前补零
+ * @param {number} num 需补零的数值
+ * @param {number} [n=2] 补零后的位数
+ * @returns {string} 补零后的字符串
+ */
+export function pad(num, n = 2) {
+  let _num = num;
+  let len = _num.toString().length;
+  while (len < n) {
+    _num = '0' + _num;
+    len++;
+  }
+  return _num;
+}
+
+/**
+ * 获取区间[min, max]的随机整数
+ * @param {number} min 区间最小值
+ * @param {number} max 区间最大值
+ * @returns {number} 区间内随机整数
+ */
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+ * 数组洗牌
+ * @param {Array} arr 需打散的数组
+ * @returns {Array} 已打散的数组
+ */
+export function shuffle(arr) {
+  const _arr = arr.slice();
+  for (let i = 0, len = _arr.length; i < len; i++) {
+    let j = getRandomInt(0, i);
+    [_arr[i], _arr[j]] = [_arr[j], _arr[i]];
+  }
+  return _arr;
+}
